712530542ee2cc142299016eb88e238303bba0a7
stropee <stropee@campus-eni.fr>
2023-12-20 14:04:42 +0100

    mod10 recycler

--- mod10recycler/src/main/java/com/example/mod10recycler/ProductAdapter.kt
+++ mod10recycler/src/main/java/com/example/mod10recycler/ProductAdapter.kt
@@ -2,11 +2,14 @@ package com.example.mod10recycler
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.mod10recycler.databinding.TemplateCourseLineBinding
 
-class ProductAdapter(val productList: List<Product>) : Adapter<ProductAdapter.ProductVH>() {
+class ProductAdapter(
+    val productList: List<Product>,
+    val listener: (name : String) -> Unit) : Adapter<ProductAdapter.ProductVH>() {
     class ProductVH(val binding: TemplateCourseLineBinding) : ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductVH {
@@ -24,6 +27,16 @@ class ProductAdapter(val productList: List<Product>) : Adapter<ProductAdapter.Pr
 
     override fun onBindViewHolder(holder: ProductVH, position: Int) {
         holder.binding.product = productList[position]
+        holder.itemView.setOnClickListener {
+            listener.invoke(productList[position].name)
+        }
+//        holder.itemView.setOnClickListener {
+//            Toast.makeText(
+//                holder.itemView.context,
+//                productList[position].toString(),
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
     }
 
 }
